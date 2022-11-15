0d18c2cd498798a3dcb5351036f0b28be275d011
Mark Howison <mhowison@ripl.org>
2022-11-15 15:49:07 -0500

    bump version to 0.3.0

--- sockit/__init__.py
+++ sockit/__init__.py
@@ -3,4 +3,4 @@ Sockit is a natural-language processing toolkit for modeling structured
 occupation information and Standard Occupational Classification (SOC) codes
 in unstructured text from job titles, job postings, and resumes.
 """
-__version__ = "0.2.1"
+__version__ = "0.3.0"
